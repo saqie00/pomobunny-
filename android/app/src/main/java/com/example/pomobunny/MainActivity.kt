bb69eeac6f43714a23f67ddd9e75f5365f75e3eb
saqie00 <hakim99saqie@gmail.com>
2025-04-21 14:39:04 +0530

    Update MainActivity.kt

--- android/app/src/main/java/com/example/pomobunny/MainActivity.kt
+++ android/app/src/main/java/com/example/pomobunny/MainActivity.kt
@@ -1,9 +1,6 @@
-package
-com.example.pomobunny
+package com.example.pomobunny
 
-import
-io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity:
-FlutterActivity() {
+class MainActivity: FlutterActivity() {
 }
